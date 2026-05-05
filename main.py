68adc3facf2dbb95d81134e45863f028de3cf646
Erik Manuel <2526_eriksaldana@iticbcn.cat>
2026-05-05 20:21:42 +0200

    Creacion de agente basico a partir de pydantic

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 from pydantic_ai import Agent
+from pydantic_ai.models.ollama import OllamaModel
+from pydantic_ai.providers.ollama import OllamaProvider
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.getenv('OLLAMA_URL')
+
+model = OllamaModel(
+    'gemma4:31b', provider=OllamaProvider(base_url=url)
+    # system_prompt=(
+    #     'Actúa como un experto en moda para describir ropa, como si estuvieras hablando directamente con el cliente'
+    # )
+)
+
+agent = Agent(model)
+
+result_sync = agent.run_sync('Desarrolla la formula de MRUA')
+
+print(result_sync.output)
