349d85a9906639e0c65f3baaf6deb445dfb6cc72
Erik Manuel <2526_eriksaldana@iticbcn.cat>
2026-05-11 22:30:23 +0200

    Add comments for a better understanding

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from pydantic import BaseModel
 from typing import Literal
 import logging
 
+# Mostramos los logs en caso de que haya un error
 logging.basicConfig(level=logging.INFO)
 
+# Cargamos las variables de entorno
 load_dotenv()
 url = os.getenv('OLLAMA_LOCAL')
 
+# Categorias para la definición de las prendas de vestir
 Categoria = Literal[
     # Prendas principales
     'vestido', 'chaqueta', 'pantalón', 'camisa', 'camiseta', 'blusa', 
@@ -26,21 +29,27 @@ Categoria = Literal[
     'otro'
 ]
 
+# Las guardamos en una variable para poder pasarselo al agente
 categorias_permitidas = Categoria.__args__
 
+# Clase DescripcionRpopa para definir las partes que tendrá la descripción de la ropa
 class DescripcionRopa(BaseModel):
     descripcion: str
     categoria: list[Categoria]
     estilo: str
 
+# Creamos el modelo
 model = OllamaModel(
     'gemma4:e4b',
     provider=OllamaProvider(base_url=url),
 )
 
+
 with open('./data/unnamed.png', 'rb') as f:
     image_bytes = f.read()
 
+
+# Creamos el agente pasandole el modelo, el output que queremos y el system_prompt para definir la manera en la que queremos que el agente responda
 agent = Agent(
     model,
     output_type=DescripcionRopa,
@@ -52,10 +61,13 @@ agent = Agent(
     'No inventes categorías nuevas.'
 )
 
+# Le pasamos la imagen al agente para que haga la descripción
 result = agent.run_sync([
     BinaryContent(data=image_bytes, media_type='image/png'),
 ])
 
+# Guardamos la respuesta del agente
 datos = result.output
 
+# Mostramos el JSON creado por el agente
 print(datos.model_dump_json(indent=2))
