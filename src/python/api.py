46e671f99a0cf8f96ea9b0e9ac07772fdd23ac18
alepina <alpimarti10@gmail.com>
2026-05-22 18:27:53 +0200

    deploy

--- src/python/api.py
+++ src/python/api.py
@@ -61,4 +61,4 @@ async def index():
 # 8. Ejecución del servidor
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
